610e2adfc30c01b12becbe65c21cfecc95e16a98
mirhajian <abolfazlmirhajian04@gmail.com>
2026-06-14 18:32:02 +0330

    Fixed turtle.py filename to avoid conflict.

---
+++ Mohammad-Amin/array_2.py
@@ -0,0 +1,16 @@
+# list = [ ]
+# list = [ "item1", "item2", "item3", etc]
+# list = [ 10, 12, 14, etc ]
+
+# tuple = ( ) 
+# tuple = ( "item1", "item2", "item3", etc )
+
+# set = { }
+# set = { 1, 2, 2, 4, 5, 6, 6, 6 }
+# print (set)
+# OUTPUT: 1, 2, 4, 5, 6
+# set = { "item1", "item1", "item1", "item2", "item3" }
+# OUTPUT: item1, item2, item3
+
+# dict = { k:v }
+# dict = { "key": "value", "key2": "value2", "key3": "value3"}
---
+++ Mohammad-Amin/tic_tac_toe.py
@@ -0,0 +1,13 @@
+import turtle as t
+
+t.pendown()
+
+t.forward(300)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.forward(300)
+
+t.penup()
